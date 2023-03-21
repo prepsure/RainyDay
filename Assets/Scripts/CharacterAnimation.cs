742953d83805d6d9f2af6dcf961d2540b00437cb
Paige <33643911+presssssure@users.noreply.github.com>
2023-03-20 21:09:45 -0400

    more

---
+++ Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    public AnimationClip _brellaOpen;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            // play animation ONCE
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            // set to frame 0 and stop playing animations
+        }
+    }
+}
